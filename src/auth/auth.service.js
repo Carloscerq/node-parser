284694db537551afbb3a0ee6b69cf7f00a610b49
Carlos Cerqueira <ce.cesc01@gmail.com>
2022-04-30 02:45:29 -0300

    feat: add Jwt middleware to movies routes

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -5,49 +5,53 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 class AuthService {
-    static async login(username, password) {
-        const userResp = await UserService.getUserFromEmail(username);
-        const user = userResp[0];
-        if (!user) {
-            throw new Error("User not found");
-        }
-
-        const isValid = await bcrypt.compare(password, user.get("data").password);
-        if (!isValid) {
-            throw new Error("Invalid password");
-        }
-
-        const token = await this.generateToken(user);
-        return Promise.resolve(token);
+  static async login(username, password) {
+    const userResp = await UserService.getUserFromEmail(username);
+    const user = userResp[0];
+    if (!user) {
+      throw new Error("User not found");
     }
 
-    static async generateToken(user) {
-        const token = jwt.sign({
-            id: user.id,
-            email: user.get("data").email
-        }, process.env.JWT_SECRET);
-
-        return Promise.resolve(token);
+    const isValid = await bcrypt.compare(password, user.get("data").password);
+    if (!isValid) {
+      throw new Error("Invalid password");
     }
 
-    static async JwtMiddleware(req, res, next) {
-        const authorization = req.headers.authorization.split(" ");
-        const token = authorization[1];
+    const token = await this.generateToken(user);
+    return Promise.resolve(token);
+  }
 
-        if (!token) {
-            return res.status(401).send("Unauthorized");
-        }
+  static async generateToken(user) {
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.get("data").email,
+      },
+      process.env.JWT_SECRET
+    );
 
-        try {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            const user = await UserService.getUserFromEmail(decoded.email);
-            req.user = user;
-            next();
-        } catch (error) {
-            console.log(error)
-            return res.status(401).send("Unauthorized");
-        }
+    return Promise.resolve(token);
+  }
+
+  static async JwtMiddleware(req, res, next) {
+    const authorization = req.headers.authorization;
+
+    if (!authorization) {
+      return res.status(401).send("Unauthorized");
     }
+
+    const token = authorization.split(" ")[1];
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await UserService.getUserFromEmail(decoded.email);
+      req.user = user;
+      next();
+    } catch (error) {
+      console.log(error);
+      return res.status(401).send("Unauthorized");
+    }
+  }
 }
 
 module.exports = AuthService;
--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,32 +1,33 @@
 const express = require("express");
 const Movies = require("./movies.entity");
 const MoviesService = require("./movies.service");
+const AuthService = require("../auth/auth.service");
 const router = express.Router();
 
 // Create a new movie
-router.post("/", (req, res) => {
+router.post("/", AuthService.JwtMiddleware, (req, res) => {
   const movie = Movies.createNewMovie(req.body);
   res.status(201).json(movie).send();
 });
 
-router.get("/", (req, res) => {
+router.get("/", AuthService.JwtMiddleware, (req, res) => {
   MoviesService.getAllMovies()
     .then((movies) => res.status(200).json(movies))
     .catch((err) => res.status(500).json(err));
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", AuthService.JwtMiddleware, (req, res) => {
   MoviesService.deleteMovie(req.params.id);
   res.status(200).send();
 });
 
-router.get("/:title", (req, res) => {
+router.get("/:title", AuthService.JwtMiddleware, (req, res) => {
   MoviesService.getMovieFromTitle(req.params.title)
     .then((movies) => res.status(200).json(movies))
     .catch((err) => res.status(500).json(err));
 });
 
-router.get("/date/:date", (req, res) => {
+router.get("/date/:date", AuthService.JwtMiddleware, (req, res) => {
   const date = req.params.date.replaceAll('-', '/');
   MoviesService.getMovieFromDate(date)
     .then((movies) => res.status(200).json(movies))
